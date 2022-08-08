9ecde9ff2ae161e493c5f262443b4813fe3c3705
BASAVARAJ HADIMANI <106008685+basavarajh99@users.noreply.github.com>
2022-08-08 20:17:39 +0530

    Update Intro.cpp

--- Intro.cpp
+++ Intro.cpp
@@ -3,7 +3,7 @@
 //overlapping subproblems are not recomputed (unlike recursion) again if already computed.
 
 //fibonacci
-
+//Memoization TC: O(N) SC: O(N)+O(N)
 int fib(int n, vector<int>& dp)
 {
   if(n<=1) return n;
@@ -12,3 +12,25 @@ int fib(int n, vector<int>& dp)
   
   return dp[n] = fib(n-1, dp)+fib(n-2, dp);
 }
+
+//Tabulation TC: O(N) SC: O(N) //no recursion stack space
+
+int fib(int n, vector<int>& dp)
+{
+  // if(n<=1) return n; convert this case as
+  dp[0] = 0, dp[1] = 1;  
+  //prev     //prev2
+  //return dp[n] = fib(n-1, dp)+fib(n-2, dp); convert this case as
+  for(int i=2; i<=n; i++)
+    dp[i] = dp[i-1] + dp[i-2];
+  //cur     //prev    //prev2
+  
+  //prev2 = prev
+  //prev = cur
+  
+  //SC:O(1)
+  
+  return dp[n-1];
+  //return dp[prev]
+}
+
